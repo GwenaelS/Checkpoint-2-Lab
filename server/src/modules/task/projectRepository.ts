fc62efa3c4009a96b551e849e08a53427cdb7cfc
Gwenael Samyn <samyngwenaelpro@gmail.com>
2026-03-25 13:36:18 +0100

    Implement CRUD operations for task management in TaskRepository and define TTask type

---
+++ server/src/modules/task/projectRepository.ts
@@ -0,0 +1,74 @@
+import databaseClient from "../../../database/client";
+
+import type { Result, Rows } from "../../../database/client";
+import type { TTask } from "../../types/TTask";
+
+class TaskRepository {
+  // The C of CRUD - Create operation
+
+  async create(task: Omit<TTask, "id" | "created_at">) {
+    // Execute the SQL INSERT query to add a new task to the "task" table
+    const [result] = await databaseClient.query<Result>(
+      "insert into task (title, description, status, user_id, project_id) values (?, ?, ?, ?)",
+      [
+        task.title,
+        task.description,
+        task.status,
+        task.user_id,
+        task.project_id,
+      ],
+    );
+
+    // Return the ID of the newly inserted task
+    return result.insertId;
+  }
+
+  // The Rs of CRUD - Read operations
+
+  async read(id: number) {
+    // Execute the SQL SELECT query to retrieve a specific task by its ID
+    const [rows] = await databaseClient.query<Rows>(
+      "select * from task where id = ?",
+      [id],
+    );
+
+    // Return the first row of the result, which represents the task
+    return rows[0] as TTask;
+  }
+
+  async readAll() {
+    // Execute the SQL SELECT query to retrieve all tasks from the "task" table
+    const [rows] = await databaseClient.query<Rows>("select * from task");
+
+    // Return the array of talks
+    return rows as TTask[];
+  }
+
+  // The U of CRUD - Update operation
+
+  async update(task: Omit<TTask, "id" | "created_at">, id: number) {
+    // Execute the SQL UPDATE operation to update a task
+    const [result] = await databaseClient.query<Result>(
+      "update task set title = ?, description = ?, status = ?, user_id = ? where id = ?",
+      [task.title, task.description, task.status, task.user_id, id],
+    );
+
+    // Return the affected rows after the SQL request
+    return result.affectedRows;
+  }
+
+  // The D of CRUD - Delete operation
+
+  async delete(id: number) {
+    // Execute the SQL DELETE operation to delete a specific task by its ID
+    const [result] = await databaseClient.query<Result>(
+      "delete from task where id = ?",
+      [id],
+    );
+
+    // Return the affected rows after the SQL request
+    return result.affectedRows;
+  }
+}
+
+export default new TaskRepository();
---
+++ server/src/types/TTask.ts
@@ -0,0 +1,9 @@
+export type TTask = {
+  id: number;
+  title: string;
+  description: string;
+  status: string;
+  created_at: Date;
+  user_id: number;
+  project_id: number;
+};
