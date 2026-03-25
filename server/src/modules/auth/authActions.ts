b9fc4cafecc705525df5b0e26287761175d5883f
Gwenael Samyn <samyngwenaelpro@gmail.com>
2026-03-25 15:28:06 +0100

    Add authentication login route and implement login logic

---
+++ server/src/modules/auth/authActions.ts
@@ -0,0 +1,58 @@
+import argon2 from "argon2";
+import dotenv from "dotenv";
+import type { RequestHandler } from "express";
+import jwt from "jsonwebtoken";
+import userRepository from "../user/userRepository";
+
+dotenv.config();
+
+const login: RequestHandler = async (req, res, next) => {
+  try {
+    if (!req.body || !req.body.email || !req.body.password) {
+      res.status(400).json({ information: "Informations incomplete" });
+      return;
+    }
+
+    const doesUserExist = await userRepository.readEmail(req.body.email);
+    if (!doesUserExist) {
+      res.status(404).json({ information: "User not found" });
+      return;
+    }
+
+    const userPassword = argon2.verify(
+      doesUserExist.password,
+      req.body.password,
+    );
+    if (!userPassword) {
+      res.status(400).json({ information: "Credentials incorrect" });
+      return;
+    }
+
+    const secret = process.env.APP_SECRET;
+    if (!secret) {
+      res.status(500).json({ information: "Server error 1" });
+      return;
+    }
+
+    const token = jwt.sign(
+      {
+        user_id: doesUserExist.id,
+        user_email: doesUserExist.email,
+        role: "user",
+      },
+      secret,
+    );
+    if (!token) {
+      res.status(500).json({ information: "Server error 2" });
+      return;
+    }
+
+    res.cookie("token", token);
+    res.status(200).json({ information: "Login successfull" });
+  } catch (err) {
+    // Pass any errors to the error-handling middleware
+    next(err);
+  }
+};
+
+export default { login };
--- server/src/router.ts
+++ server/src/router.ts
@@ -6,6 +6,7 @@ const router = express.Router();
 // Define Your API Routes Here
 /* ************************************************************************* */
 
+import authActions from "./modules/auth/authActions";
 import projectActions from "./modules/project/projectActions";
 import taskActions from "./modules/task/taskActions";
 import userActions from "./modules/user/userActions";
@@ -24,12 +25,15 @@ router.put("/api/projects/:id", projectActions.edit);
 router.post("/api/projects", projectActions.add);
 router.delete("/api/projects/:id", projectActions.destroy);
 
-// ========== USER ROUTES ==========
+// ========== TASK ROUTES ==========
 router.get("/api/tasks", taskActions.browse);
 router.get("/api/tasks/:id", taskActions.read);
 router.put("/api/tasks/:id", taskActions.edit);
 router.post("/api/tasks", taskActions.add);
 router.delete("/api/tasks/:id", taskActions.destroy);
+
+// ========== AUTH ROUTES ==========
+router.post("/api/login", authActions.login);
 
 /* ************************************************************************* */
 
