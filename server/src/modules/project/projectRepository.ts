fb0168dbebd66360678817fec9325bc4b6a9c0ff
Gwenael Samyn <samyngwenaelpro@gmail.com>
2026-03-25 12:52:47 +0100

    Implement CRUD operations for project management in ProjectRepository

---
+++ server/src/modules/project/projectRepository.ts
@@ -0,0 +1,71 @@
+import databaseClient from "../../../database/client";
+
+import type { Result, Rows } from "../../../database/client";
+import type { TProject } from "../../types/TProject";
+
+class ProjectRepository {
+  // The C of CRUD - Create operation
+
+  async create(project: Omit<TProject, "id" | "created_at">) {
+    // Execute the SQL INSERT query to add a new project to the "project" table
+    const [result] = await databaseClient.query<Result>(
+      "insert into project (title, description, status, created_by) values (?, ?, ?, ?)",
+      [project.title, project.description, project.status, project.created_by],
+    );
+
+    // Return the ID of the newly inserted project
+    return result.insertId;
+  }
+
+  // The Rs of CRUD - Read operations
+
+  async read(id: number) {
+    // Execute the SQL SELECT query to retrieve a specific project by its ID
+    const [rows] = await databaseClient.query<Rows>(
+      "select * from project where id = ?",
+      [id],
+    );
+
+    // Return the first row of the result, which represents the project
+    return rows[0] as TProject;
+  }
+
+  async readAll() {
+    // Execute the SQL SELECT query to retrieve all projects from the "project" table
+    const [rows] = await databaseClient.query<Rows>("select * from project");
+
+    // Return the array of projects
+    return rows as TProject[];
+  }
+
+  // The U of CRUD - Update operation
+
+  async update(
+    project: Omit<TProject, "id" | "created_at" | "created_by">,
+    id: number,
+  ) {
+    // Execute the SQL UPDATE operation to update a project
+    const [result] = await databaseClient.query<Result>(
+      "update project set title = ?, description = ?, status = ? where id = ?",
+      [project.title, project.description, project.status, id],
+    );
+
+    // Return the affected rows after the SQL request
+    return result.affectedRows;
+  }
+
+  // The D of CRUD - Delete operation
+
+  async delete(id: number) {
+    // Execute the SQL DELETE operation to delete a specific project by its ID
+    const [result] = await databaseClient.query<Result>(
+      "delete from project where id = ?",
+      [id],
+    );
+
+    // Return the affected rows after the SQL request
+    return result.affectedRows;
+  }
+}
+
+export default new ProjectRepository();
---
+++ server/src/types/TProject.ts
@@ -0,0 +1,8 @@
+export type TProject = {
+  id: number;
+  title: string;
+  description: string;
+  status: string;
+  created_at: Date;
+  created_by: number;
+};
